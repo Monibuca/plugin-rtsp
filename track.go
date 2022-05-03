96834e26a11d69f7cd3e906265b402d03ad6b28e
langhuihui <178529795@qq.com>
2022-05-03 22:24:07 +0800

    加上空格

--- track.go
+++ track.go
@@ -24,5 +24,5 @@ func (t *TrackPCMA) MediaDescription() *psdp.MediaDescription {
 }
 
 func NewH265Track(payloadType uint8, sprop [][]byte) (gortsplib.Track, error) {
-	return gortsplib.NewTrackGeneric("video", []string{fmt.Sprintf("%d", payloadType)}, fmt.Sprintf("%d H265/90000", payloadType), fmt.Sprintf("%d packetization-mode=1;sprop-vps=%s;sprop-sps=%s;sprop-pps=%s;", payloadType, base64.StdEncoding.EncodeToString(sprop[0]), base64.StdEncoding.EncodeToString(sprop[1]), base64.StdEncoding.EncodeToString(sprop[2])))
+	return gortsplib.NewTrackGeneric("video", []string{fmt.Sprintf("%d", payloadType)}, fmt.Sprintf("%d H265/90000", payloadType), fmt.Sprintf("%d packetization-mode=1; sprop-vps=%s; sprop-sps=%s; sprop-pps=%s;", payloadType, base64.StdEncoding.EncodeToString(sprop[0]), base64.StdEncoding.EncodeToString(sprop[1]), base64.StdEncoding.EncodeToString(sprop[2])))
 }
